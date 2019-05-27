6ce9f01449994221b33fd4b33901dec693651246
Tetiana <Tatyana.granchenko@gmail.com>
2019-05-27 18:14:20 +0300

    adding alerts

--- TestSourceTreeProject/ViewController.swift
+++ TestSourceTreeProject/ViewController.swift
@@ -10,15 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func alertAction(_ sender: Any) {
+        
+        let showAlert = { [weak self] in
+            
+            let alertController = UIAlertController(title: "Hey AppCoda", message: "Whatdoyou want to do", preferredStyle: .alert)
+            
+            let handler = { (action: UIAlertAction) -> Void in print("\(String(describing: action.title))")
+            }
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: handler)
+            alertController.addAction(cancelAction)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default, handler: { action in print("\(String(describing: action.title))") })
+            alertController.addAction(okAction)
+            
+            let defaultAction = UIAlertAction(title: "Default", style: .default, handler: handler)
+            alertController.addAction(defaultAction)
+            
+            let destructiveAction = UIAlertAction(title: "Destructive", style: .destructive, handler: handler)
+            alertController.addAction(destructiveAction)
+            
+            self?.present(alertController, animated: true) { print("Alert") }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: showAlert)
+    }
+    
+    @IBAction func sheetAlertAction(_ sender: Any) {
+        
+        let showAlert = { [weak self] in
+            
+            let alertController = UIAlertController(title: "Hey AppCoda", message: "Whatdoyou want to do", preferredStyle: .actionSheet)
+            
+            let handler = { (action: UIAlertAction) -> Void in print("\(String(describing: action.title))")
+            }
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: handler)
+            alertController.addAction(cancelAction)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default, handler: { action in print("\(String(describing: action.title))") })
+            alertController.addAction(okAction)
+            
+            let defaultAction = UIAlertAction(title: "Default", style: .default, handler: handler)
+            alertController.addAction(defaultAction)
+            
+            let destructiveAction = UIAlertAction(title: "Destructive", style: .destructive, handler: handler)
+            alertController.addAction(destructiveAction)
+            
+            self?.present(alertController, animated: true) { print("Alert sheet") }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: showAlert)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
         print("viewWillApear")
     }
 }
